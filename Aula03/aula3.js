045b5f79eb8e919e8add2f49220f74a2faa5d59a
isabela <162368740+Mueller07@users.noreply.github.com>
2024-03-08 17:14:32 -0300

    Add files via upload

---
+++ Aula03/aula3.js
@@ -0,0 +1,17 @@
+//Este código imprime numa mensagem
+
+console.log('Olá' , 'Isabela')
+
+const meunome = 'Isabela'
+let idade = 16 
+const altura = 1.62
+const temperatura = -20
+
+let souUmboolean = true
+souUmBoolean = false
+
+console.log("meu nome é ",meunome,"e tenho ",idade,"anos. ")
+
+idade = 17
+
+console.log("meu nome é ",meunome,"e tenho ",idade,"anos. ")
---
+++ Aula03/pratica.js
@@ -0,0 +1,32 @@
+
+
+//let altera valor 
+//const não altera valor
+const meunome = "Isabela"
+const sobrenome = "Mueller"
+const idade = 16
+const estudante = true
+
+//este código imprime mensagens 
+console.log("meu nome é",meunome,sobrenome,"tenho",idade,"anos. Sou estudante?",estudante)
+
+const serie = "Greys Anatomy"
+const temporada = 17
+
+//este comando imprime o tipo de valor
+//pode ser string, number, boolean
+console.log(typeof serie,typeof temporada)
+
+console.log("minha séria favorita é" ,serie, typeof serie, "ela tem:", temporada, "temporadas" 
+,typeof temporada)
+
+
+const idadenumero = 35
+const idadetexto = idadenumero.toString()
+console.log(typeof idadenumero)
+console.log(typeof idadetexto)
+
+const IdadeTexto2 = "35"
+const IdadeNumero2 = Number(IdadeTexto2)
+console.log(typeof IdadeTexto2)
+console.log(typeof IdadeNumero2)
